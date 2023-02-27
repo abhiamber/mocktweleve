33eaa20d2987c4758b2d7a6f744fcb197c79e27b
abhiamber191 <abhiamber191@gmail.com>
2023-02-27 22:33:15 +0530

    cors

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 require("dotenv").config();
 let conncet = require("./config/db");
 let PORT = process.env.PORT;
-let cors = require("cors");
 app.use(express.json());
 let cors = require("cors");
 app.use(cors());
